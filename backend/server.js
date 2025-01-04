bf4a0643b0809196ffc0434f020618993d28cdd2
sarayu19724 <madigundusarayu@gmail.com>
2025-01-04 12:37:37 +0530

    ...

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,21 @@
-const express = require('express')
-const app = express()
+// server.js
+const express = require('express');
+const app = express();
+const PORT = 3006;
 
-app.
+// define the route
+app.get('/',
+    (req, res) => {
+        res.send(
+            `<h1 style="color: green;">
+            Hello Gfg!</h1>`
+        );
+    });
 
-app.listen(3000)
+app.listen(PORT,
+    () => {
+        console.log(
+            `Server is listening at 
+            http://localhost:${PORT}`
+        );
+    });
