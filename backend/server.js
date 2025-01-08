fe46b7c11e14049437c03c55c10a68e69144e839
sarayu19724 <madigundusarayu@gmail.com>
2025-01-08 11:42:53 +0530

    ...

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3006;
-
+app.use(express.json())
 // define the route
 app.get('/',
     (req, res) => {
@@ -21,4 +21,20 @@ app.listen(PORT,
     });
 // creating course
 
+app.get('/courses',(req,res)=>{
+    res.status(200).send(
+        {
+            tshirt:'',
+            szie:'large'
+        }
+    )
+})
 
+app.post('/courses/:id', (req,res)=>{
+    const {id} = req.params;
+    const{title} =req.body;
+    res.send({
+        tshirt:{title},
+        id:{id},
+    })
+});
