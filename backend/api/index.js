1de91cd9ef0bb5f586589152776d6638da5021fa
sarayu19724 <madigundusarayu@gmail.com>
2025-01-13 19:14:28 +0530

    backend

---
+++ backend/api/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+let dotenv = require("dotenv").config({path:'../.env'});
+console.log(dotenv);
+
+
+const coursesRoutes = require("./routes/courseroute");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = process.env.NEXT_PUBLIC_PORT;
+
+//routes
+app.use("/courses", coursesRoutes);
+
+app.get("/", (req, res) => res.send("Backend is running!"));
+
+app.listen(PORT,
+    () => {
+        console.log(
+            `Server is listening at 
+            http://localhost:${PORT}`
+        );
+    });
+
+module.exports = app
