2f95a571b2a8280f57cc11224105deea71559f41
sarayu19724 <madigundusarayu@gmail.com>
2025-01-14 19:25:23 +0530

    ...

--- frontend/app/signup/page.js
+++ frontend/app/signup/page.js
@@ -5,13 +5,14 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
 import { useState } from "react";
+import Link from "next/link";
 export default function Login(){
     
        
         const images = [
-          "url('https://images6.alphacoders.com/480/480421.jpg')",
-          "url('https://miro.medium.com/v2/resize:fit:1200/1*WWTBjFs6gqqsXGAsfg9nsQ.jpeg')",
-          "url('https://www.thebalancemoney.com/thmb/cfLSIpcpwiluW-kJDTu16bXNhjo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/school-books-on-desk--education-concept-871454068-5b548900c9e77c005b04fc8c.jpg')",
+          "url('https://c.wallhere.com/photos/18/38/background_solid_bright-1073142.jpg!d')",
+          
+          "url('https://wallpapercave.com/wp/wp2424412.jpg')",
         ];
         const [currentImage, setCurrentImage] = useState(0);
         const changeBackground = () => {
@@ -25,7 +26,7 @@ export default function Login(){
         <div className="box" style={{zIndex:"1000",border:'1px solid black',width:'450px',height:'500px',display:'grid',placeItems:'center',backgroundColor:'white'}} onClick={(e) => e.stopPropagation()}>
             <h1 style={{marginBottom:'20px',display:'flex',justifyContent:'center',marginTop:'40px',color:'black'}}>Login</h1>
             <form style={{width:'400px',height:'500px',color:'black'}}>
-                <Grid container spacing={6}>
+                <Grid container spacing={5}>
                    <Grid  item xs={12}>
                     <TextField 
                      label="Username*"
@@ -44,12 +45,12 @@ export default function Login(){
                    </Grid>
                 </Grid>
                 <FormControlLabel  control={<Checkbox />} label="Remember me" style={{marginLeft:'2px'}}/>
-                <Button type="submit" style={{marginTop:'100px',backgroundColor:' #4F86F7',color:'white'}}>Login</Button>
+                <Button type="submit" style={{marginTop:'90px',backgroundColor:' #4F86F7',color:'white'}}>Login</Button>
             <Grid container style={{marginTop:'20px'}}>
               <Grid item xs >
                 
-                <Link href="/signup" variant="body2" style={{color:' #4F86F7',marginTop:'-2',textDecoration: 'none'}}>
-                Don't have an account? Sign Up
+                <Link href="/login" variant="body2" style={{color:' #4F86F7',marginTop:'-2',textDecoration: 'none'}}>
+                Already have an account? Sign In
                 </Link>
                 
               </Grid>
